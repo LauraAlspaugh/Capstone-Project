ddbfe01eefadd10dc542cdea30febaf501f98797
Laura <lauraalspaugh2002@icloud.com>
2023-11-07 16:37:02 -0700

    change

--- server/src/controllers/RoutinesController.js
+++ server/src/controllers/RoutinesController.js
@@ -7,8 +7,44 @@ export class RoutinesController extends BaseController {
         super('api/routines')
         this.router
             .get('', this.getRoutines)
+            .get('/:routineId', this.getRoutineById)
             .use(Auth0Provider.getAuthorizedUserInfo)
             .post('', this.createRoutine)
+            .put('/:routineId', this.editRoutine)
+            .delete('/:routineId', this.archiveRoutine)
+    }
+    async archiveRoutine(request, response, next) {
+        try {
+            const routineId = request.params.routineId
+            const userId = request.userInfo.id
+            const routine = await routinesService.archiveRoutine(routineId, userId)
+            return response.send(routine)
+        } catch (error) {
+            next(error)
+
+        }
+    }
+    async editRoutine(request, response, next) {
+        try {
+            const routineId = request.params.routineId
+            const userId = request.userInfo.id
+            const routineData = request.body
+            const updatedRoutine = await routinesService.editRoutine(routineId, userId, routineData)
+            return response.send(updatedRoutine)
+        } catch (error) {
+            next(error)
+
+        }
+    }
+    async getRoutineById(request, response, next) {
+        try {
+            const routineId = request.params.routineId
+            const routine = await routinesService.getRoutineById(routineId)
+            return response.send(routine)
+        } catch (error) {
+            next(error)
+
+        }
     }
     async createRoutine(request, response, next) {
         try {
--- server/src/services/RoutinesService.js
+++ server/src/services/RoutinesService.js
@@ -1,6 +1,47 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest, Forbidden } from "../utils/Errors.js"
 
 class RoutinesService {
+    async archiveRoutine(routineId, userId) {
+        const routine = await this.getRoutineById(routineId)
+        if (routine.creatorId.toString() != userId) {
+            throw new Forbidden('not your routine to archive!')
+        }
+        routine.isArchived = !routine.isArchived
+        await routine.save()
+        return ('this routine has been archived!')
+    }
+    async editRoutine(routineId, userId, routineData) {
+        const routineToBeUpdated = await this.getRoutineById(routineId)
+        if (routineToBeUpdated.creatorId.toString() != userId) {
+            throw new BadRequest('do not even try it')
+        } if (!routineToBeUpdated) {
+            throw new BadRequest('this is not a routine to be updated')
+
+        }
+        routineToBeUpdated.name = routineData.name || routineToBeUpdated.name
+        routineToBeUpdated.description = routineData.description || routineToBeUpdated.description
+        routineToBeUpdated.category = routineData.category || routineToBeUpdated.category
+        routineToBeUpdated.target = routineData.target || routineToBeUpdated.target
+        routineToBeUpdated.isPrivate = routineData.isPrivate != undefined ? routineData.isPrivate :
+            routineToBeUpdated.isPrivate
+        routineToBeUpdated.isArchived = routineData.isArchived != undefined ? routineData.isArchived :
+            routineToBeUpdated.isArchived
+        routineToBeUpdated.level = routineData.level || routineToBeUpdated.level
+        routineToBeUpdated.moves = routineData.moves || routineToBeUpdated.moves
+        routineToBeUpdated.playCount = routineData.playCount != undefined ? routineData.playCount :
+            routineToBeUpdated.playCount
+        routineToBeUpdated.playTime = routineData.playTime != undefined ? routineData.playTime :
+            routineToBeUpdated.playTime
+        await routineToBeUpdated.save()
+        return routineToBeUpdated
+    }
+    async getRoutineById(routineId) {
+        const routine = (await dbContext.Routines.findById(routineId)).populate('creator', 'name picture')
+        if (!routine) {
+            throw new BadRequest('this is not a valid routine')
+        } return routine
+    }
     async createRoutine(routineData) {
         const newRoutine = await dbContext.Routines.create(routineData)
         await newRoutine.populate('creator', 'name picture')
