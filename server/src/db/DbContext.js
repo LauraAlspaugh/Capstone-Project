d5eb4e603c50283800ca9cb517543cd532f552e8
Laura <lauraalspaugh2002@icloud.com>
2023-11-07 12:30:35 -0700

    change

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -3,12 +3,16 @@ import { AccountSchema } from '../models/Account'
 import { RoutineSchema } from '../models/Routine.js';
 import { MoveSchema } from '../models/Move.js';
 import { ListEntrySchema } from '../models/ListEntry.js';
+import { FavoriteMoveSchema } from '../models/FavoriteMove.js';
+import { FavoriteRoutineSchema } from '../models/FavoriteRoutine.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Routines = mongoose.model('Routine', RoutineSchema);
   Moves = mongoose.model('Move', MoveSchema);
   ListEntries = mongoose.model('ListEntry', ListEntrySchema);
+  FavoriteMoves = mongoose.model('FavoriteMove', FavoriteMoveSchema);
+  FavoriteRoutines = mongoose.model('FavoriteRoutine', FavoriteRoutineSchema);
 
 }
 
