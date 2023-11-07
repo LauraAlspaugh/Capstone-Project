06850bd686d9bba3c643d44333b57b646b47e0a3
Laura <lauraalspaugh2002@icloud.com>
2023-11-07 14:17:46 -0700

    change

--- server/src/services/RoutinesService.js
+++ server/src/services/RoutinesService.js
@@ -1,4 +1,17 @@
+import { dbContext } from "../db/DbContext.js"
+
 class RoutinesService {
+    async createRoutine(routineData) {
+        const newRoutine = await dbContext.Routines.create(routineData)
+        await newRoutine.populate('creator', 'name picture')
+        return newRoutine
+
+    }
+    async getRoutines(query) {
+        const moves = await dbContext.Routines.find(query).populate('creator', 'name picture')
+        return moves
+
+    }
 
 }
 export const routinesService = new RoutinesService()
