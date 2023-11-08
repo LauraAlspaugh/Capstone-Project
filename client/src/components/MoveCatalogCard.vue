b240890178a940173d8857f69b2237de9d1f1997
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-08 16:48:16 -0700

    img fix

--- client/src/components/MoveCatalogCard.vue
+++ client/src/components/MoveCatalogCard.vue
@@ -1,36 +1,36 @@
 <template>
-    <!-- <section class="row">
-        <div class="col-10"> -->
-    <section class="row white-gb">
-        <div class="col-3">
-            <img :src="moveProp.imgUrl" alt="moveProp.englishName">
-        </div>
-        <div class="col-9">
-            <section class="row">
-                <div class="col d-flex justify-content-between">
-                    <p>{{ moveProp.englishName }}</p>
-                    <p>{{ moveProp.level }}</p>
-                    <p>{{ moveProp.bodyPart }}</p>
-                    <!-- TODO add v-if to show closed in heart if the logged in user has favorited that pose -->
-                    <p><i class=" fs-2 mdi mdi-heart-outline"></i></p>
-                    <!-- <p><i class="fs-2 mdi mdi-heart"></i></p> -->
+    <section class="row">
+        <div class="col-10">
+            <section class="row white-gb">
+                <div class="col-3">
+                    <img :src="moveProp.imgUrl" alt="moveProp.englishName">
                 </div>
-            </section>
-            <section class="row">
-                <div class="col">
-                    {{ moveProp.sanskritName }}
-                </div>
-            </section>
-            <section class="row">
-                <div class="col">
-                    <p>Benefits:</p>
-                    <li>{{ moveProp.benefitsArray }}</li>
+                <div class="col-9">
+                    <section class="row">
+                        <div class="col d-flex justify-content-between">
+                            <p>{{ moveProp.englishName }}</p>
+                            <p>{{ moveProp.level }}</p>
+                            <p>{{ moveProp.bodyPart }}</p>
+                            <!-- TODO add v-if to show closed in heart if the logged in user has favorited that pose -->
+                            <p><i class=" fs-2 mdi mdi-heart-outline"></i></p>
+                            <!-- <p><i class="fs-2 mdi mdi-heart"></i></p> -->
+                        </div>
+                    </section>
+                    <section class="row">
+                        <div class="col">
+                            {{ moveProp.sanskritName }}
+                        </div>
+                    </section>
+                    <section class="row">
+                        <div class="col">
+                            <p>Benefits:</p>
+                            <li>{{ moveProp.benefitsArray }}</li>
+                        </div>
+                    </section>
                 </div>
             </section>
         </div>
     </section>
-    <!-- </div>
-    </section> -->
 </template>
 
 
@@ -47,4 +47,9 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+    height: 30rem;
+    width: 30rem;
+}
+</style>
