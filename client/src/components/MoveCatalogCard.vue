64fedd99c235cf8e54fd9a1bc7ac8ffb64224ad1
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-08 16:02:09 -0700

    working on move catalog card

--- client/src/components/MoveCatalogCard.vue
+++ client/src/components/MoveCatalogCard.vue
@@ -1,5 +1,17 @@
 <template>
-    <img :src="moveProp.imgUrl" alt="moveProp.englishName">
+    <!-- <section class="row">
+        <div class="col-10"> -->
+    <section class="row">
+        <div class="col-3">
+            <img :src="moveProp.imgUrl" alt="moveProp.englishName">
+        </div>
+        <div class="col-9">
+            
+        </div>
+    </section>
+    <!-- </div>
+    </section> -->
+
     <p>{{ moveProp.englishName }}</p>
     <p>{{ moveProp.description }}</p>
 </template>
@@ -10,14 +22,12 @@ import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
 import { Move } from '../models/Move.js';
 export default {
-    props: {moveProp: {type: Move, required: true}},
-    setup(){
-    return {  }
+    props: { moveProp: { type: Move, required: true } },
+    setup() {
+        return {}
     }
 };
 </script>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
