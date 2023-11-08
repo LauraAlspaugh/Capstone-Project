a4321fd2e84211449421295e26e9e195005820b8
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-08 15:45:15 -0700

    white gb

--- client/src/App.vue
+++ client/src/App.vue
@@ -67,4 +67,13 @@ footer {
 .poretOne {
   font-family: 'Poiret One', sans-serif;
 }
+
+.white-gb {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.38);
+  border-radius: 8px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.6px);
+  -webkit-backdrop-filter: blur(5.6px);
+}
 </style>
