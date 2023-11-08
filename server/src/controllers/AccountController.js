bba6457e1d85073fdfdd1a82377a91efe2d1b8ae
Corinna Bolon <kyndrill@rocketmail.com>
2023-11-07 17:39:16 -0700

    Favorite Move functions

--- server/src/controllers/AccountController.js
+++ server/src/controllers/AccountController.js
@@ -1,15 +1,28 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
 import BaseController from '../utils/BaseController'
+import { favoritesService } from "../services/FavoritesService.js"
 
 export class AccountController extends BaseController {
   constructor() {
     super('account')
     this.router
+      .get('/favorites/moves', this.getMyFavoritedMoves)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
       .put('', this.updateUserAccount)
       .post('', this.dailyActivity)
+  }
+
+  async getMyFavoritedMoves(request, response, next) {
+    try {
+      const userId = request.userInfo.id
+      const myFavoritedMoves = await favoritesService.getMyFavoritedMoves(userId)
+      return response.send(myFavoritedMoves)
+    } catch (error) {
+      next(error)
+    }
+
   }
 
   async getUserAccount(req, res, next) {
