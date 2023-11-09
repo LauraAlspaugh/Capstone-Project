79aa91938674bce84baa618452822fe32112c98a
DJ Aaron <koreangeekman@gmail.com>
2023-11-09 09:53:17 -0700

    home & about basic frame

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
-  </div>
+    <div class="container-fluid">
+        <section class="row justify-content-center">
+          <div class="col-12 d-flex justify-content-center p-3">
+            <p class="">About the App & Devs</p>
+          </div>
+        </section>
+        <section class="row justify-content-center">
+          <div class="col-12 col-md-5 p-3 order-1 order-md-1 d-flex justify-content-center">
+            <img src="" alt="img for dev 1" class="img1">
+          </div>
+          <div class="col-12 col-md-7 p-3 order-2 order-md-2 d-flex justify-content-center">
+            <p class="">dev 1</p>
+          </div>
+          <div class="col-12 col-md-7 p-3 order-4 order-md-3 d-flex justify-content-center">
+            <p class=""> dev 2</p>
+          </div>
+          <div class="col-12 col-md-5 p-3 order-3 order-md-4 d-flex justify-content-center">
+            <img src="" alt="img for dev 2" class="img2">
+          </div>
+        </section>
+        <section class="row justify-content-center">
+          <div class="col-12 col-md-5 p-3 order-1 order-md-1 d-flex justify-content-center">
+            <img src="" alt="img for dev 3" class="img3">
+          </div>
+          <div class="col-12 col-md-7 p-3 order-2 order-md-2 d-flex justify-content-center">
+            <p class=""> dev 3</p>
+          </div>
+          <div class="col-12 col-md-7 p-3 order-4 order-md-3 d-flex justify-content-center">
+            <p class=""> dev 2</p>
+          </div>
+          <div class="col-12 col-md-5 p-3 order-3 order-md-4 d-flex justify-content-center">
+            <img src="" alt="img for dev 2" class="img2">
+          </div>
+        </section>
+      </div>
 </template>
 
 <script>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,33 @@
 <template>
-  <p class="fs-1 text-center p-4">Welcome to Rooted Flow</p>
+  <div class="container-fluid">
+      <section class="row justify-content-center">
+        <div class="col-12 d-flex justify-content-center p-3">
+          <img src="../assets/img/RootedFlow Logo-lg.png" alt="Rooted Flow Logo" class="bigLogo rounded-circle">
+        </div>
+      </section>
+      <section class="row justify-content-center">
+        <div class="col-12 col-md-7 p-3 order-2 order-md-1 d-flex justify-content-center">
+          <p class="">text 1</p>
+        </div>
+        <div class="col-12 col-md-5 p-3 order-1 order-md-2 d-flex justify-content-center">
+          <img src="" alt="img for text 1" class="img1">
+        </div>
+        <div class="col-12 col-md-5 p-3 order-3 order-md-3 d-flex justify-content-center">
+          <img src="" alt="img for text 2" class="img2">
+        </div>
+        <div class="col-12 col-md-7 p-3 order-4 order-md-4 d-flex justify-content-center">
+          <p class=""> text 2</p>
+        </div>
+      </section>
+      <section class="row justify-content-center">
+        <div class="col-12 col-md-7 p-3 order-2 order-md-1 d-flex justify-content-center">
+          <p class=""> text 3</p>
+        </div>
+        <div class="col-12 col-md-5 p-3 order-1 order-md-2 d-flex justify-content-center">
+          <img src="" alt="img for text 3" class="img3">
+        </div>
+      </section>
+    </div>
 </template>
 
 <script>
@@ -11,23 +39,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.bigLogo{
+  box-shadow: 0 0 1rem .25rem white;
 }
 </style>
