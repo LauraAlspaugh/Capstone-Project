25045920c9f4a601f2cfe424dea6c23889875583
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-08 17:29:46 -0700

    kicked out

--- client/src/components/MoveCatalogCard.vue
+++ client/src/components/MoveCatalogCard.vue
@@ -1,29 +1,28 @@
 <template>
     <section class="row justify-content-center">
         <div class="col-10 py-3">
-            <section class="row white-gb">
+            <section class="row white-gb align-items-center p-3">
                 <div class="col-3">
                     <img class="img-fluid" :src="moveProp.imgUrl" alt="moveProp.englishName">
                 </div>
                 <div class="col-9">
                     <section class="row">
-                        <div class="col d-flex justify-content-between">
-                            <span>{{ moveProp.englishName }}</span>
-                            <span>{{ moveProp.level }}</span>
-                            <span>{{ moveProp.sanskritName }}</span>
+                        <div class="col d-block d-md-flex justify-content-between align-items-center">
+                            <span class="fs-3 m-0">{{ moveProp.englishName }}</span>
+                            <p class="m-0">{{ moveProp.level }}</p>
                             <!-- TODO add v-if to show closed in heart if the logged in user has favorited that pose -->
                             <span><i class=" fs-2 mdi mdi-heart-outline"></i></span>
                             <!-- <p><i class="fs-2 mdi mdi-heart"></i></p> -->
                         </div>
                     </section>
-                    <section class="row">
+                    <section class="row my-2">
                         <div class="col">
-                            <span v-for="focus in moveProp.bodyPart" :key="focus"> {{ focus }}</span>
+                            <span class="pe-4" v-for="focus in moveProp.bodyPart" :key="focus"> {{ focus }}</span>
                         </div>
                     </section>
                     <section class="row">
-                        <div class="col">
-                            <p>Benefits:</p>
+                        <div class="col pb-2">
+                            <p class="m-0">Benefits:</p>
                             <li v-for="benefit in moveProp.benefitsArray" :key="benefit">{{ benefit }}</li>
                         </div>
                     </section>
