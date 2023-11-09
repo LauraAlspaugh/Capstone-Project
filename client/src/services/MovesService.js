35bbced5dc3f877dcae8a0eeb4b78f165d70ba3d
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-09 15:17:41 -0700

    taking off loggers

--- client/src/services/MovesService.js
+++ client/src/services/MovesService.js
@@ -7,19 +7,19 @@ import { api } from "./AxiosService.js"
 class MovesService{
 async getMoves(){
     const res = await api.get('api/moves')
-    logger.log('getting moves!', res.data)
+    // logger.log('getting moves!', res.data)
     AppState.moves = res.data.map(pojo => new Move(pojo))
 }
 async getMoveById(moveId){
     AppState.activeMove = null
     const res = await api.get(`api/moves/${moveId}`)
-    logger.log('getting move by Id', res.data)
+    // logger.log('getting move by Id', res.data)
     AppState.activeMove = new Move(res.data)
 }
 
 async favoriteMove(moveId){
     const res = await api.post(`api/favorites/moves`, {moveId})
-    logger.log(res.data)
+    // logger.log(res.data)
     AppState.myFavoriteMoves.push(new FavoriteMove(res.data))
 }
 
@@ -34,7 +34,7 @@ async unfavoriteMove(moveId){
 
 async getMyFavoriteMoves(){
     const res = await api.get(`api/favorites/moves`)
-    logger.log('get my favorite moves', res.data)
+    // logger.log('get my favorite moves', res.data)
 
     AppState.myFavoriteMoves = res.data.map(fav => new FavoriteMove(fav))
 }
