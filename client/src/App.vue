c5348190d4108c79ce51f4bd584eb1ad6216e7a3
Erin Shiffler <erinmaeshiffler@gmail.com>
2023-11-10 14:11:46 -0700

    modal clickable inside now

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,13 +10,23 @@
   <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
   <div class="modal fade" id="move-modal" tabindex="-1" data-bs-keyboard="false" role="dialog"
     aria-labelledby="modalTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-top modal-dialog-scrollable modal-md" role="document">
-      <div v-if="activeMove">
-        <MoveDetailCard />
+    <div class="modal-dialog modal-dialog-top modal-dialog-scrollable modal-lg" role="document">
+      <div class="modal-content">
+        <!-- <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div> -->
+
+        <div class="modal-body p-0" v-if="activeMove">
+          <MoveDetailCard />
+          <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
+        </div>
       </div>
+
     </div>
   </div>
 </template>
+
+
 
 <script>
 import { computed } from 'vue'
