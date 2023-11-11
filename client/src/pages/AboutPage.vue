670bc2b090a716a15391aea14bd9bb1c17f2217e
DJ <koreangeekman@gmail.com>
2023-11-11 08:29:16 -0700

    home & about tweaks

--- client/src/pages/AboutPage.vue
+++ client/src/pages/AboutPage.vue
@@ -1,54 +1,82 @@
 <template>
-    <div class="container-fluid">
-        <section class="row justify-content-center p-3">
-          <div class="col-12 d-flex justify-content-center text-center py-3 px-5">
-            <p class="">About the App & Devs <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum, ab sequi architecto rem accusamus? Repellat voluptatibus repudiandae, unde dicta cum incidunt? Quibusdam explicabo distinctio repellendus consequatur tenetur numquam deleniti.</p>
-          </div>
-        </section>
-        <section class="row justify-content-center p-3">
-          <div class="col-12 col-md-5 p-3 my-2 order-1 order-md-1 d-flex justify-content-center">
-            <img src="" alt="img for dev 1" class="img1">
-          </div>
-          <div class="col-12 col-md-7 p-3 my-2 rounded order-2 order-md-2 d-flex justify-content-center desc">
-            <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime itaque iusto numquam, excepturi facilis voluptatibus voluptates vero in? Iste voluptatem praesentium consequuntur nobis. Ad, officiis quisquam. Totam quaerat dignissimos eius!</p>
-          </div>
-          <div class="col-12 col-md-7 p-3 my-2 rounded order-4 order-md-3 d-flex justify-content-center desc">
-            <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, architecto. Fuga ad animi earum culpa expedita dignissimos mollitia vitae, molestiae tempore. Quibusdam adipisci autem voluptate est at accusamus aperiam ipsa.</p>
-          </div>
-          <div class="col-12 col-md-5 p-3 my-2 order-3 order-md-4 d-flex justify-content-center">
-            <img src="" alt="img for dev 2" class="img2">
-          </div>
-        </section>
-        <section class="row justify-content-center">
-          <div class="col-12 col-md-5 p-3 my-2 order-1 order-md-1 d-flex justify-content-center">
-            <img src="" alt="img for dev 3" class="img3">
-          </div>
-          <div class="col-12 col-md-7 p-3 my-2 rounded order-2 order-md-2 d-flex justify-content-center desc">
-            <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, itaque accusantium. Fugiat cumque vel nesciunt alias sed vero temporibus a repellat eius iste suscipit quibusdam esse repudiandae fugit, unde in.</p>
-          </div>
-          <div class="col-12 col-md-7 p-3 my-2 rounded order-4 order-md-3 d-flex justify-content-center desc">
-            <p class="">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit cumque officia placeat nihil nostrum, perferendis amet ducimus, similique recusandae voluptatibus aspernatur culpa error quaerat ut, ipsa facere? Similique, recusandae adipisci.</p>
-          </div>
-          <div class="col-12 col-md-5 p-3 my-2 order-3 order-md-4 d-flex justify-content-center">
-            <img src="" alt="img for dev 2" class="img2">
-          </div>
-        </section>
+  <div class="container-fluid">
+    <section class="row justify-content-center p-3">
+      <div class="col-12 text-center p-3 my-5 white-gb w-50">
+        <p class="fs-1 p-3 rounded">About the App & Devs </p>
+        <p class="fs-5 p-2 rounded">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum, ab sequi
+          architecto rem accusamus? Repellat voluptatibus repudiandae, unde dicta cum incidunt? Quibusdam explicabo
+          distinctio repellendus consequatur tenetur numquam deleniti.</p>
+        <p class="fs-5 p-2 rounded">Express Node.js, Vue 3, Bootstrap 5.3.2, Mongoose, MongoDB (...)</p>
       </div>
+    </section>
+    <section class="row justify-content-center align-items-center px-5">
+      <div class="col-12 col-md-5 p-3 my-2 order-1 order-md-1 d-flex justify-content-center">
+        <img src="" alt="img for dev 1" class="img border p-2">
+      </div>
+      <div class="col-12 col-md-7 p-3 my-2 rounded order-2 order-md-2 d-flex justify-content-center desc">
+        <p class="fs-5 p-2 rounded white-gb">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime itaque iusto
+          numquam, excepturi facilis voluptatibus voluptates vero in? Iste voluptatem praesentium consequuntur nobis. Ad,
+          officiis quisquam. Totam quaerat dignissimos eius!</p>
+      </div>
+      <div class="col-12 col-md-7 p-3 my-2 rounded order-4 order-md-3 d-flex justify-content-center desc">
+        <p class="fs-5 p-2 rounded white-gb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci,
+          architecto. Fuga ad animi earum culpa expedita dignissimos mollitia vitae, molestiae tempore. Quibusdam adipisci
+          autem voluptate est at accusamus aperiam ipsa.</p>
+      </div>
+      <div class="col-12 col-md-5 p-3 my-2 order-3 order-md-4 d-flex justify-content-center">
+        <img src="" alt="img for dev 2" class="img border p-2">
+      </div>
+    </section>
+    <section class="row justify-content-center align-items-center px-5">
+      <div class="col-12 col-md-5 p-3 my-2 order-1 order-md-1 d-flex justify-content-center">
+        <img src="" alt="img for dev 3" class="img border p-2">
+      </div>
+      <div class="col-12 col-md-7 p-3 my-2 rounded order-2 order-md-2 d-flex justify-content-center desc">
+        <p class="fs-5 p-2 rounded white-gb">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique, itaque
+          accusantium. Fugiat cumque vel nesciunt alias sed vero temporibus a repellat eius iste suscipit quibusdam esse
+          repudiandae fugit, unde in.</p>
+      </div>
+      <div class="col-12 col-md-7 p-3 my-2 rounded order-4 order-md-3 d-flex justify-content-center desc">
+        <p class="fs-5 p-2 rounded white-gb">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit cumque
+          officia placeat nihil nostrum, perferendis amet ducimus, similique recusandae voluptatibus aspernatur culpa
+          error quaerat ut, ipsa facere? Similique, recusandae adipisci.</p>
+      </div>
+      <div class="col-12 col-md-5 p-3 my-2 order-3 order-md-4 d-flex justify-content-center">
+        <img src="" alt="img for dev 2" class="img border p-2">
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 
 
 export default {
-    setup() {
-        return {};
-    },
-    // components: { MoveDetailCard, RoutineDetailCard, MoveSearchComponent }
+  setup() {
+    return {};
+  },
+  // components: { MoveDetailCard, RoutineDetailCard, MoveSearchComponent }
 }
 </script>
 
 <style scoped lang="scss">
-.desc {
-  background-color: #ffffff69;
+img {
+  box-shadow:
+    0 0 1rem .25rem white,
+    0 0 2rem .5rem white inset;
+  background-color: #ffffffa8;
+}
+
+.img {
+  height: 16rem;
+}
+
+.white-gb {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.38);
+  border-radius: 8px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.6px);
+  -webkit-backdrop-filter: blur(5.6px);
 }
 </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,36 +1,36 @@
 <template>
   <div class="container-fluid">
     <section class="row justify-content-center">
-      <div class="col-12 d-flex justify-content-center p-3">
+      <div class="col-12 d-flex justify-content-center py-5">
         <img src="../assets/img/RootedFlow Logo-lg.png" alt="Rooted Flow Logo" class="bigLogo rounded-circle pt-5 px-3">
       </div>
     </section>
-    <section class="row justify-content-center">
+    <section class="row justify-content-center align-items-center pt-3">
       <div class="col-12 col-md-7 py-3 px-5 order-2 order-md-1 d-flex justify-content-center">
-        <p class="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis veniam temporibus quod, tenetur
-          accusantium alias voluptas? Iure, vel magni. Mollitia maiores vero totam, quaerat rem a rerum accusamus minima
-          saepe.</p>
+        <p class="fs-5 p-2 rounded">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis veniam temporibus quod,
+          tenetur accusantium alias voluptas? Iure, vel magni. Mollitia maiores vero totam, quaerat rem a rerum accusamus
+          minima saepe.</p>
       </div>
       <div class="col-12 col-md-5 p-3 order-1 order-md-2 d-flex justify-content-center">
-        <img src="" alt="img for text 1" class="img1">
+        <img src="" alt="img for text 1" class="img border p-2">
       </div>
       <div class="col-12 col-md-5 p-3 order-3 order-md-3 d-flex justify-content-center">
-        <img src="" alt="img for text 2" class="img2">
+        <img src="" alt="img for text 2" class="img border p-2">
       </div>
       <div class="col-12 col-md-7 py-3 px-5 order-4 order-md-4 d-flex justify-content-center">
-        <p class=""> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vel a ipsum sunt necessitatibus,
-          architecto aliquid vero provident accusamus ad distinctio nemo consequatur recusandae officiis non veniam, est
-          cum? Quasi.</p>
+        <p class="fs-5 p-2 rounded"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae vel a ipsum sunt
+          necessitatibus, architecto aliquid vero provident accusamus ad distinctio nemo consequatur recusandae officiis
+          non veniam, est cum? Quasi.</p>
       </div>
     </section>
-    <section class="row justify-content-center">
+    <section class="row justify-content-center align-items-center">
       <div class="col-12 col-md-7 py-3 px-5 order-2 order-md-1 d-flex justify-content-center">
-        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet ipsa nisi voluptates
-          asperiores quaerat, repellat id ut totam expedita, neque dolorem error perspiciatis. Fugit aperiam animi
-          veritatis unde non.</p>
+        <p class="fs-5 p-2 rounded">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus amet ipsa nisi
+          voluptates asperiores quaerat, repellat id ut totam expedita, neque dolorem error perspiciatis. Fugit aperiam
+          animi veritatis unde non.</p>
       </div>
       <div class="col-12 col-md-5 p-3 order-1 order-md-2 d-flex justify-content-center">
-        <img src="" alt="img for text 3" class="img3">
+        <img src="" alt="img for text 3" class="img border p-2">
       </div>
     </section>
   </div>
@@ -45,11 +45,32 @@ export default {
 </script>
 
 <style scoped lang="scss">
+img {
+  box-shadow:
+    0 0 1rem .25rem white,
+    0 0 2rem .5rem white inset;
+  background-color: #ffffffa8;
+}
+
+.img {
+  height: 16rem;
+}
+
 .bigLogo {
   box-shadow:
     0 0 1rem .25rem white,
     0 0 2rem .5rem white inset;
-  background-color: #ffffffb9;
+  background-color: #ffffffa8;
   backdrop-filter: blur(2px);
+}
+
+.white-gb,
+p {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.38);
+  border-radius: 8px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5.6px);
+  -webkit-backdrop-filter: blur(5.6px);
 }
 </style>
